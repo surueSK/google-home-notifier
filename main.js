c6ec156dfd1037b04f3c956c94edeaef584f61a9
surueSK <35964012+surueSK@users.noreply.github.com>
2019-01-03 13:14:43 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const googlehome = require('google-home-notifier')
 const language = 'ja';
 
-googlehome.device('Google-Home', language);
+googlehome.device('Google-Home', language); 
 
 googlehome.notify('こんにちは。私はグーグルホームです。', function(res) {
   console.log(res);
